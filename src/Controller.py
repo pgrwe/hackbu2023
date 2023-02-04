943b228841a9b624230563d00157b559f079f99a
Madeline Scotti <madelinescotti@Madelines-MacBook-Pro.local>
2023-02-04 17:37:41 -0500

    Trying to make screens

--- src/Controller.py
+++ src/Controller.py
@@ -16,13 +16,31 @@ class Controller:
         self.screen = pygame.display.set_mode((self.window_width, self.window_height))
         self.background = pygame.Surface((self.window_width, self.window_height))
         pygame.display.set_caption("Cat Valentine Adventure")
-        pygame.key.set_repeat(50, 500)
+        pygame.key.set_repeat(10, 50)
         self.state = "GAME"
 
         self.player = Player.Player()
         self.enemy = Enemy.Enemy("Ghosty", 700, 350,)
         self.all_sprites = pygame.sprite.Group((self.player),(self.enemy))
 
+        self.buttonimg = pygame.image.load("assets/redButton.png")
+        self.button = pygame.transform.scale(self.buttonimg, (150, 150))
+
+    def mainLoop(self):
+        """
+        If game is still running, runs gameLoop; if game is over(player health is at 0), runs gameover; if game is at a savepoint, runs savepoint; if player won(enemy health is at 0), runs win
+        args: None
+        return: None
+        """
+        while True:
+            if (self.state == "GAME"):
+                self.gameLoop()
+            elif (self.state == "TITLE"):
+                self.title()
+            # elif (self.state == "WIN"):
+            #     self.win()
+            # elif (self.state == "GAMEOVER"):
+            #     self.gameOver()
 
     def gameLoop(self):
         """
@@ -47,5 +65,66 @@ class Controller:
                 self.player.move()
             self.all_sprites.draw(self.screen)
             pygame.display.flip()
+            self.screen.fill("black")
         self.all_sprites.draw(self.screen)
         pygame.display.flip()
+
+    def title(self):
+        """
+        description: Sets up title screen with it text, buttons, and images.
+        args: None
+        return: None
+        """
+        while self.state == "TITLE":
+            pygame.font.init()
+            title1 = font.render('Cat Valentine', True, (131, 139, 139))
+            title2 = font.render('Adventure', True, (131, 139, 139))
+            start = font.render('Start', True, (131, 139, 139))
+            exit = font.render('Exit', True, (131, 139, 139))
+
+            button_start = pygame.Rect((25, 155), (150, 40))
+            button_exit = pygame.Rect((25, 235), (150, 40))
+
+            self.screen.blit(title1, (20, 10))
+            self.screen.blit(title2, (40, 75))
+            self.screen.blit(start, (40, 150))
+            self.screen.blit(exit, (40, 175))
+
+            self.screen.blit(self.button, (25, 160))
+            self.screen.blit(self.button, (25, 208))
+
+            click = False
+            mx, my = pygame.mouse.get_pos()
+
+            for event in pygame.event.get():
+                event.type == pygame.mouse.get_pressed()
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if event.button == 1:
+                        click = True
+
+            if button_NG.collidepoint((mx, my)):
+                if click:
+                    self.state = "GAME"
+
+            if button_Exit.collidepoint((mx, my)):
+                if click:
+                    self.exitGame()
+
+            pygame.display.update()
+
+    #
+    # def win(self):
+    #     while self.state == "WIN":
+    #
+    # def gameOver(self):
+    #     while self.state == "GAMEOVER":
+
+    def exitGame(self):
+        """
+        description: stops running program ("Exits game")
+        args: None
+        return: None
+        """
+        pygame.quit()
+        sys.exit
+        pygame.display.update()
--- src/Player.py
+++ src/Player.py
@@ -8,7 +8,10 @@ class Player(pygame.sprite.Sprite):
         return: None
         """
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load("assets/cat.png").convert_alpha()
+        # self.image = pygame.image.load("assets/cat.png").convert_alpha()
+        self.image = pygame.Surface([10,20])
+        self.image.fill("white")
+
         self.rect = self.image.get_rect()
         self.rect.x = 500
         self.rect.y = 350
@@ -16,6 +19,7 @@ class Player(pygame.sprite.Sprite):
         self.name = "Val"
         self.rect = pygame.Rect((self.rect.x,self.rect.y),(10,20))
         self.speed = 15
+        self.lives = 9
 
     def move(self, direction = None):
         """
@@ -25,17 +29,17 @@ class Player(pygame.sprite.Sprite):
         """
         if direction == "U":
           self.direction = "U"
-          self.image = pygame.image.load("assets/cat.png")
+          # self.image = pygame.image.load("assets/cat.png")
           self.rect.y -= self.speed
         elif direction == "D":
           self.direction = "D"
-          self.image = pygame.image.load("assets/cat.png")
+          # self.image = pygame.image.load("assets/cat.png")
           self.rect.y += self.speed
         elif direction == "L":
           self.direction = "L"
           self.rect.x -= self.speed
-          self.image = pygame.image.load("assets/cat.png")
+          # self.image = pygame.image.load("assets/cat.png")
         elif direction == "R":
           self.direction = "R"
           self.rect.x += self.speed
-          self.image = pygame.image.load("assets/cat.png")
+          # self.image = pygame.image.load("assets/cat.png")
