1d8ff0ca353efc59a63eb76db179761919b49fc6
Madeline Scotti <madelinescotti@Madelines-MacBook-Pro.local>
2023-02-04 15:28:12 -0500

    added player class

---
+++ src/Player.py
@@ -0,0 +1,10 @@
+import pygame
+class Player(pygame.sprite.Sprite):
+  def __init__ (self):
+    """
+    description: Function initializes the player state and the sprite.
+    args: None
+    return: None
+    """
+    pygame.sprite.Sprite.__init__(self)
+    
